3d0f9285254751c85bff8d0ccb9281c9cb4d0f3d
Kirill <kirill.zayats.99@mail.ru>
2024-01-02 14:05:00 +0300

    refactor (extended 1.1-1.2): add semantic tags and alternative contant for images

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ const createNewTaskElement = function (taskString) {
 
   deleteButton.className = "delete";
   deleteButtonImg.src = "./remove.svg";
+  deleteButtonImg.alt = "Icon remove";
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
